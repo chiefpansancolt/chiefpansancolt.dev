822bb4c80fd4417ea531e05116f2b54207386e50
Christopher Pezza <chris.pezza@hey.com>
2023-03-25 20:32:13 -0400

    add tag

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -29,6 +29,21 @@ export default function Document(props) {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Lexend:wght@400;500&display=swap"
         />
+
+        <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `,
+            }}
+          />
       </Head>
       <body className="flex h-full flex-col">
         <Main />
